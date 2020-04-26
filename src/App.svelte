6b0ebf29ae20b908c69d755d65390c90fcaf1372
GlevDev <garth@glev.fr>
2020-04-26 11:33:49 +0200

    add local storage and modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,15 @@
 <script>
-  import { setContext } from "svelte";
+  import { setContext, onMount, afterUpdate } from "svelte";
   // components
   import Navbar from "./Navbar.svelte";
   import ExpensesList from "./ExpensesList.svelte";
   import Totals from "./Totals.svelte";
   import ExpenseForm from "./ExpenseForm.svelte";
+  import Modal from "./Modal.svelte";
   // data
-  import expensesData from "./expenses";
+  // import expensesData from "./expenses";
   // variables
-  let expenses = [...expensesData];
+  let expenses = [];
   // set editing variables
   let setName = "";
   let setAmount = null;
@@ -20,16 +21,16 @@
   $: total = expenses.reduce((acc, curr) => {
     return (acc += curr.amount);
   }, 0);
-	// functions
-	function showForm() {
-		isFormOpen = true;
-	}
-	function hideForm() {
-		isFormOpen = false;
-		setName = '';
-		setAmount = null;
-		setId = null;
-	}
+  // functions
+  function showForm() {
+    isFormOpen = true;
+  }
+  function hideForm() {
+    isFormOpen = false;
+    setName = "";
+    setAmount = null;
+    setId = null;
+  }
   function removeExpense(id) {
     expenses = expenses.filter(item => item.id !== id);
   }
@@ -48,8 +49,8 @@
     let expense = expenses.find(item => item.id === id);
     setId = expense.id;
     setName = expense.name;
-		setAmount = expense.amount;
-		showForm();
+    setAmount = expense.amount;
+    showForm();
   }
   function editExpense({ name, amount }) {
     expenses = expenses.map(item => {
@@ -62,18 +63,32 @@
   // context
   setContext("remove", removeExpense);
   setContext("modify", setModifiedExpense);
+  // local storage
+  function setLocalStorage() {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }
+  onMount(() => {
+    expenses = localStorage.getItem("expenses")
+      ? JSON.parse(localStorage.getItem("expenses"))
+      : [];
+  });
+  afterUpdate(() => {
+    setLocalStorage();
+  });
 </script>
 
 <Navbar {showForm} />
 <main class="content">
   {#if isFormOpen}
-    <ExpenseForm
-      {addExpense}
-      name={setName}
-      amount={setAmount}
-      {isEditing}
-      {editExpense}
-			{hideForm} />
+    <Modal>
+      <ExpenseForm
+        {addExpense}
+        name={setName}
+        amount={setAmount}
+        {isEditing}
+        {editExpense}
+        {hideForm} />
+    </Modal>
   {/if}
   <Totals title="total des dépenses" {total} />
   <ExpensesList {expenses} />
--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -15,6 +15,7 @@
     }
     name = "";
     amount = null;
+    hideForm();
   }
 </script>
 
